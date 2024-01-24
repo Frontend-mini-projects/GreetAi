73e6df577711a7d098e9abcfdd833879926b7fff
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2024-01-24 10:12:06 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,254 @@
+console.log('Welcome to Greet.AI');
+
+// birthday card 1 form
+function bdaycard1(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLScAnIYRISJWR6qNlMuGIYHNQR8xHdqOtzzGoPoOJlpWdJMF5w/viewform?embedded=true";
+    
+}
+function bdaycard2(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSf3n7gHGDlAmTfYyAWCOK-EAbyVIN4EKBcFPaCoKQUTseWhSg/viewform?embedded=true";
+    
+}
+function bdaycard3(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSdNgDkQOoAIIAd8gOgvOvRqXDBG-QPv0g6JQoPOpus-XKwvzg/viewform?embedded=true";
+    
+}
+function bdaycard4(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSf3ksRNHpS9saWqpTs6pGA_AlA6JoltBjTZUBcFpv_uidbYAQ/viewform?embedded=true";
+    
+}
+function bdaycard5(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSdNMJ3DsHFalb81RgRhbLEZhJgCjBkUqS4e31sx65v1ND5Y9w/viewform?embedded=true";
+    
+}
+function bdaycard6(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSeYuQjlW_Nkl4tpFsCAFQQ1d-av7aLJZNW4oz56qoM8LSS90Q/viewform?embedded=true";
+    
+}
+function bdaycard7(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLScjZi_5GtnogHPLlpOElJMbTpq5yLDmMAFtwPP-VA6MoBG2zA/viewform?embedded=true";
+    
+}
+function bdaycard8(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSdCdC21bfnJn4TgF6TEzYx4Z4siphC2wGPlAtRYt-WEHZj7Hw/viewform?embedded=true";
+    
+}
+function bdaycard9(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSeIjn8bWG0yYG8oezo8HWenDVAKnw954yRO9L79PVFpfSCjhA/viewform?embedded=true";
+    
+}
+function bdaycard10(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSfECmsNUdv1R9MKpACSR1gYxRR3xfg3jfSPA4Ki_xsyOAytVw/viewform?embedded=true";
+    
+}
+function bdaycard11(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSdGMb-_h-Jn6UQ3JpQheoT642eC8eKHq0KMVtHBZf9UjuByZQ/viewform?embedded=true";
+    
+}
+function bdaycard12(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSfHEvARIqmhq7HbdZ_Sej6kU_dJy7kcoGIaqLgqVFhRamonyQ/viewform?embedded=true";
+    
+}
+function bdaycard13(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSeRK501ddDDFs894zQoWZ64hLbA_eB-sHQB-P2LJZCR_G830Q/viewform?embedded=true";
+    
+}
+function bdaycard14(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLScOaZdDRLlYi_ANnXbHF9VBcve5NeuThLg6ENpmzrz7298n0g/viewform?embedded=true";
+    
+}
+function bdaycard15(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSfggcEpPZRhrETQh6kj7SukxEsrT4Yhwir3oVfcjH9E5DvjuQ/viewform?embedded=true";
+    
+}
+function bdaycard16(){
+    let f = document.getElementById("test1");
+    // console.log(`Link is: ${f.src}`);
+    
+    f.src = "https://docs.google.com/forms/d/e/1FAIpQLSeKL95XqRd4CvHR0Pmz6Y5Jl44ZubNnkMSaGnXBPdDXswXXNw/viewform?embedded=true";
+    
+}
+
+
+
+
+
+function allbday(){
+    let content = document.getElementById("bday");
+    content.innerHTML = ` <div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding" >Template 1</div> -->
+        <img src="img/bday/card1.jpg" alt="card1" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard1();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 2</div> -->
+        <img src="img/bday/card2.jpg" alt="card2" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard2();"></a>
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 3</div> -->
+        <img src="img/bday/card3.jpg" alt="card3" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard3();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 4</div> -->
+        <img src="img/bday/card4.jpg" alt="card4" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard4();">
+    </div>
+</div>
+
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 5</div> -->
+        <img src="img/bday/card5.jpg" alt="card5" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard5();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 6</div> -->
+        <img src="img/bday/card6.jpg" alt="card6" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard6();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 7</div> -->
+        <img src="img/bday/card7.jpg" alt="card7" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard7();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 8</div> -->
+        <img src="img/bday/card8.jpg" alt="card8" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard8();">
+    </div>
+</div>
+
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 9</div> -->
+        <img src="img/bday/card9.jpg" alt="card9" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard9();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 10</div> -->
+        <img src="img/bday/card10.jpg" alt="card10" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard10();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 11</div> -->
+        <img src="img/bday/card11.jpg" alt="card11" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard11();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 12</div> -->
+        <img src="img/bday/card12.jpg" alt="card12" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard12();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 13</div> -->
+        <img src="img/bday/card13.jpg" alt="card13" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard13();">
+    </div>
+</div>
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 14</div> -->
+        <img src="img/bday/card14.jpg" alt="card14" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard14();">
+    </div>
+</div>
+
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 15</div> -->
+        <img src="img/bday/card15.jpg" alt="card15" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard15();">
+    </div>
+</div>
+
+<div class="w3-col l3 m6 w3-margin-bottom">
+    <div class="w3-display-container">
+        <!-- <div class="w3-display-topleft w3-black w3-padding">Template 16</div> -->
+        <img src="img/bday/card16.jpg" alt="card16" style="width:100%" data-toggle="modal"
+            data-target="#cardmodal" onclick="bdaycard16();">
+    </div>
+</div>
+
+
+`;
+}
+
+
+
+
+// mail contact js function
+function groupmail(){
+    window.open(`mailto:surpriseyourslove@gmail.com`);
+    // windo.open('mailto:surpriseyourslove@gmail.com');
+  }
+
+
+
